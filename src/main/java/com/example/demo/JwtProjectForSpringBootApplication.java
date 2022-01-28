486110b87d98db7b86b9dec27c7022e4b29a3ace
Ezra <ezra.dev98@gmail.com>
2022-01-28 18:23:56 +0300

    CRUD impl v2..

--- src/main/java/com/example/demo/JwtProjectForSpringBootApplication.java
+++ src/main/java/com/example/demo/JwtProjectForSpringBootApplication.java
@@ -1,7 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.Entity.Role;
+import com.example.demo.Entity.User;
+import com.example.demo.Service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class JwtProjectForSpringBootApplication {
@@ -9,5 +16,27 @@ public class JwtProjectForSpringBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JwtProjectForSpringBootApplication.class, args);
 	}
+	@Bean
+	CommandLineRunner run(UserService userService) {
+		return args -> {
+			userService.saveRole(new Role(1, "ROLE_USER"));
+			userService.saveRole(new Role(2, "ROLE_MANAGER"));
+			userService.saveRole(new Role(3, "ROLE_ADMIN"));
+			userService.saveRole(new Role(4, "ROLE_SUPER_ADMIN"));
+
+			userService.saveUser(new User(1, "John Travolta", "john", "1234", new ArrayList<>()));
+			userService.saveUser(new User(2, "Will Smith", "will", "1234", new ArrayList<>()));
+			userService.saveUser(new User(3, "Jim Carry", "jim", "1234", new ArrayList<>()));
+			userService.saveUser(new User(4, "Arnold Schwarzenegger", "arnold", "1234", new ArrayList<>()));
+
+			userService.addRoleToUser("john", "ROLE_USER");
+			userService.addRoleToUser("will", "ROLE_MANAGER");
+			userService.addRoleToUser("jim", "ROLE_ADMIN");
+			userService.addRoleToUser("arnold", "ROLE_SUPER_ADMIN");
+			userService.addRoleToUser("arnold", "ROLE_ADMIN");
+			userService.addRoleToUser("arnold", "ROLE_USER");
+		};
+	}
+
 
 }
