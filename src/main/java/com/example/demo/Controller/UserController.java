56fc97502d54004d736348e41bdb1d912c2cffc9
Ezra <ezra.dev98@gmail.com>
2022-01-28 18:04:32 +0300

    CRUD impl..

--- src/main/java/com/example/demo/Controller/UserController.java
+++ src/main/java/com/example/demo/Controller/UserController.java
@@ -1,13 +1,15 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Entity.Role;
 import com.example.demo.Entity.User;
 import com.example.demo.Service.UserService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,4 +24,36 @@ public class UserController {
         return ResponseEntity.ok().body(userService.getAllUsers());
 
     }
+
+    @PostMapping("/user/save")
+    public ResponseEntity<User> saveUser(@RequestBody User user){
+
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+
+    }
+
+    @PostMapping("/role/save")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role){
+
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+
+    }
+
+    @PostMapping("/role/addtouser")
+    public ResponseEntity<?> addRoleToUser(@RequestBody RoleToUserForm form){
+
+        userService.addRoleToUser(form.getUserEmail(), form.getRoleName());
+        return ResponseEntity.ok().build();
+
+    }
+
+
 }
+   @Data
+   class RoleToUserForm{
+        private String userEmail;
+        private String roleName;
+   }
+
