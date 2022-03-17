6cf3b7b70fa572bbfda2616e911fa2022154297b
Ezra <82878692+ezralangat88@users.noreply.github.com>
2022-03-17 10:11:29 +0300

    Update ResourceNotFoundException.java

--- src/main/java/com/example/demo/Exception/ResourceNotFoundException.java
+++ src/main/java/com/example/demo/Exception/ResourceNotFoundException.java
@@ -2,7 +2,7 @@ package com.example.demo.Exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
+//ResourceNotFoundException.
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException{
 
